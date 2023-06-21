8f6cceb663f21f6612a9249e72bd07b355462cc8
Piyush Garg <107670320+piyush8188@users.noreply.github.com>
2023-06-21 16:29:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  // TODO: Send the ""Hello, world!"" response
+ res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world!');
 });
 
 server.listen(port, () => {
+console.log(`Server running at http://localhost:${port}/`);
   // TODO: Log a message to the console when the server starts listening on the port
 });
